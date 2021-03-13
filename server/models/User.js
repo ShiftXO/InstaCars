2838b5956c83afb506c178760f5aad3b46fbd270
Vladimir Mollov <vladimirmolav@abv.bg>
2021-03-13 20:17:33 +0200

    Add full name to User model.

--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    fullName: {
+        type: String,
+        required: true,
+    },
     username: {
         type: String,
         required: true,
