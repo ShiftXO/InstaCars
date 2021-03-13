bdc5d2d07b9dcfc569ac2b4fbc33b1c020398d61
Vladimir Mollov <vladimirmolav@abv.bg>
2021-03-13 20:44:39 +0200

    Call mongoose in server startup.

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const routes = require('./routes');
 const cors = require('cors');
 const { auth } = require('./middlewares/auth');
 const config = require('./config/index');
+const mongoose = require('./config/mongoose');
 
 const app = express();
+mongoose();
 app.use(cors());
 app.use(express.json());
 app.use(auth);
-
 app.get('/', (req, res) => {
     res.json({ message: 'OMEGALUL' });
 });
