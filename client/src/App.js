cccdfe7eb8fc92bef50cf281966e2d95875c1f2d
Vladimir Mollov <vladimirmolav@abv.bg>
2021-03-19 20:26:46 +0200

    Add routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Route, Link, NavLink, Redirect, Switch } from 'react-router-dom';
+
+import { Container, Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Card from "./components/Card";
+import AppBar from "./components/AppBar";
+import TransitionsModal from "./components/Create";
+import ImageUploadCard from "./components/ImageUpload";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const useStyles = makeStyles((theme) => ({
+    appBar: {
+      marginBottom: "110px"
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route path="/" exact>
+        <Grid className={classes.appBar}>
+          <AppBar />
+        </Grid>
+        <Grid container direction={'column'} alignItems={'center'}>
+          <Card />
+        </Grid>
+      </Route>
+      <Route path="/login" exact component={SignIn} />
+      <Route path="/register" exact component={SignUp} />
+    </>
   );
 }
 
