12a6469544398e541bee20141446b1b5c69eea9f
Vladimir Mollov <vladimirmolav@abv.bg>
2021-03-21 19:30:24 +0200

    Add user route.

--- server/routes.js
+++ server/routes.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 
 const postController = require('./controllers/postController');
 const authController = require('./controllers/authController');
+const userController = require('./controllers/userController');
 
 const router = Router();
 
 router.use('/post', postController);
 router.use('/auth', authController);
+router.use('/user', userController);
 router.get('*', (req, res) => {
     res.render('404');
 });
