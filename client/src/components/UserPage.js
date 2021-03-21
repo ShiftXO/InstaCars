e71ba487e9f755f143789a7f895b20b34fb54e9f
Vladimir Mollov <vladimirmolav@abv.bg>
2021-03-21 19:35:51 +0200

    Fetch user data.

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -13,6 +13,7 @@ import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from "@material-ui/icons/Info";
 import Avatar from '@material-ui/core/Avatar';
 import { Divider, Grid, GridList, GridListTile } from '@material-ui/core';
+import authService from '../services/authService';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -45,59 +46,71 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function UserPage() {
+export default function UserPage(props) {
     const classes = useStyles();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log(props.match.params.id);
+            const data = await authService.getUser(props.match.params.id);
+            console.log(data.result);
+            if (data.result) {
+                setUser(data.result);
+            }
+        };
+
+        fetchData();
+    }, [])
 
     return (
         <>
-            <Grid container direction="row" justify="center"  >
-                <Grid item xs={2} >
-                    <Avatar alt="Remy Sharp" className={classes.large} src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg" />
-                </Grid>
-                <Grid container item xs={4} justify="center" >
-                    <Grid container spacing={3} direction="row" justify="flex-start" >
-                        <Grid item>
-                            <Typography variant="h5" className={classes.username}>Billi Hlapeto</Typography>
+            {user != null ?
+                (
+                    <>
+                        <Grid container direction="row" justify="center" >
+                            <Grid item xs={2} >
+                                <Avatar alt="Remy Sharp" className={classes.large} src={user.profileImage} />
+                            </Grid>
+                            <Grid container item xs={4} justify="center" >
+                                <Grid container spacing={3} direction="row" justify="flex-start" >
+                                    <Grid item>
+                                        <Typography variant="h5" className={classes.username}>{user.username}</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" size="small" color="primary">follow</Button>
+                                    </Grid>
+                                </Grid>
+                                <Grid container spacing={4}>
+                                    <Grid item>
+                                        <b>{user.posts.length} </b>
+                                        <span> posts</span>
+                                    </Grid>
+                                    <Grid item>
+                                        <b>{user.followers.length} </b>
+                                        <span>followers</span>
+                                    </Grid>
+                                    <Grid item>
+                                        <b>{user.following.length} </b>
+                                        <span>following</span>
+                                    </Grid>
+                                </Grid>
+                                <Grid container item>
+                                    <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
+                                </Grid>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Button variant="contained" size="small" color="primary">follow</Button>
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={4}>
-                        <Grid item>
-                            <b>230 </b>
-                            <span> posts</span>
-                        </Grid>
-                        <Grid item>
-                            <b>1.5m </b>
-                            <span>followers</span>
-                        </Grid>
-                        <Grid item>
-                            <b>23 </b>
-                            <span>following</span>
-                        </Grid>
-                    </Grid>
-                    <Grid container item>
-                        <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id nulla id elit gravida accumsan. Morbi odio nunc, euismod eget urna vitae, molestie elementum ex. Pellentesque nec tellus turpis. Nulla sagittis magna at finibus placerat. Duis nisi odio, ullamcorper eu gravida at, facilisis sed urna. Phasellus eu orci vel leo vulputate euismod. Proin egestas urna dui, non blandit massa eleifend ut. Donec a pharetra mauris. In varius tristique nisi, quis vestibulum lectus auctor nec. Nullam lorem purus, facilisis in arcu sit amet, aliquam malesuada ipsum. Mauris malesuada turpis bibendum justo viverra egestas.</Typography>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Divider component="hr" variant="fullWidth" />
+                        <Divider component="hr" variant="fullWidth" />
 
-            <Grid className={classes.imageContainer} container >
-                <Grid item xs={4} >
-                    <img className={classes.image} src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg" />
-                </Grid>
-                <Grid item xs={4} >
-                    <img className={classes.image} src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg" />
-                </Grid>
-                <Grid item xs={4} >
-                    <img className={classes.image} src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg" />
-                </Grid>
-                <Grid item xs={4} >
-                    <img className={classes.image} src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg" />
-                </Grid>
-            </Grid>
+                        <Grid className={classes.imageContainer} container >
+                            {user.posts.map(post =>
+                                <Grid key={post._id} item xs={4} >
+                                    <img className={classes.image} src={post.imageUrl} />
+                                </Grid>
+                            )}
+                        </Grid>
+                    </>
+                ) : ('')}
         </>
     );
 }
